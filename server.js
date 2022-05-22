052603a69234c5d9d5f92b56dfa69d42f5141f9f
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-22 23:09:43 +0930

    handlebar works

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.engine('handlebars', extendexhdbInstance.engine);
 app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    // res.send("Hello World")
+    res.render('home')
 })
 
 app.listen(5000)
