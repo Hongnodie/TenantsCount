eab347b7b82c7cfc79a6c06209cd67588c113bdd
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-22 18:43:51 +0930

    handlebar frame init

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const express = require('express')
+const express = require('express');
+const exhdbInstance = require('express-handlebars');
+
 const app = express();
+
+// Set up Handlebars.js engine with customised view functions
+const extendexhdbInstance = exhdbInstance.create({});
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', extendexhdbInstance.engine);
+app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
     res.send("Hello World")
