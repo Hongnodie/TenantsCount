0a51e6c13a4dc27318a5eb6b31fa19241a1aef5c
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-23 14:39:05 +0930

    Abort original and apply mern template

--- other/dbConnect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/tenantscount', {
-    // Should eliminate deprecation warnings
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-module.exports = mongoose.connection;
