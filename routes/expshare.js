9ea9d5c80a3b5981ebfb5d2f13672fdf4d05e511
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-23 10:53:32 +0930

    create thread page framing

--- routes/expshare.js
+++ routes/expshare.js
@@ -1,8 +1,29 @@
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+    // res.send("Hello World")
+    const expthreads = [{
+      title: 'Test experience 1',
+      createdAt: new Date(),
+      description: 'Test description 1',
+    },
+    {
+      title: 'Test experience 2',
+      createdAt: new Date(),
+      description: 'Test description 2',
+    }];
+    res.render('./blogSection/blogHomePage', {
+      expthreads: expthreads
+    })
+})
+
 router.get('/new', (req, res) => {
     res.render('./blogSection/blogNewThread');
     // res.send('router result - Blog section main page');
-})
+});
+
+router.post('/', (req, res) => {
+
+});
 
 module.exports = router;
