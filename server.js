77065c3877d19abc8a6bdc204cdb74e70bbbc3a8
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-23 10:19:22 +0930

    expshare route

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const exhdbInstance = require('express-handlebars');
 
+const expRouter = require('./routes/expshare');
+
 const app = express();
 
 // Set up Handlebars.js engine with customised view functions
@@ -8,11 +10,20 @@ const extendexhdbInstance = exhdbInstance.create({});
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', extendexhdbInstance.engine);
-app.set('view engine', 'handlebars')
+app.set('view engine', 'handlebars');
+
+app.use('/expshare', expRouter);
 
 app.get('/', (req, res) => {
     // res.send("Hello World")
-    res.render('home')
+    const expthreads = [{
+      title: 'Test experience',
+      createdAt: Date.now(),
+      description: 'Test',
+    }];
+    res.render('blogPage', {
+      ...expthreads,
+    })
 })
 
 app.listen(5000)
