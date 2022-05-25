9f17c24c4f4d05aec05d190121356bec1f8e2f68
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-25 22:36:51 +0930

    jwt token init

--- Server/Routes/auth.js
+++ Server/Routes/auth.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const User = require("../Models/User");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-router.post("/register", async (req, res, next) => {
+router.post("/register", async (req, res) => {
     try {
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password,salt);
@@ -19,15 +20,23 @@ router.post("/register", async (req, res, next) => {
 	}
 })
 
-router.post("/login", async (req, res, next) => {
+router.post("/login", async (req, res) => {
     try {
+        // find the user by username
         const user = await User.findOne({username:req.body.username});
         if(!user) return res.status(500).json("user not found");
 
+        // verify the password
         const passwordCorrect = await bcrypt.compare(req.body.password, user.password);
         if(!passwordCorrect) return res.status(500).json("password is incorrect");
 
-		res.status(200).json(user);
+        // hide password and admin condition from user
+        const {password, isAdmin, ...otherDetails} = user._doc;
+
+        // generate token to save endless login efforts when jumping from page to page
+
+        // send as json object
+		res.status(200).json({otherDetails});
 	} catch (error) {
 		res.status(500).json(error);
 	}
