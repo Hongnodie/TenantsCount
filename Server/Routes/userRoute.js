37f41c3aea0630cd715aed3d1edafb275ba93c7a
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-25 22:50:08 +0930

    User route init and delete create

--- Server/Routes/userRoute.js
+++ Server/Routes/userRoute.js
@@ -1,7 +1,50 @@
 const router = require("express").Router();
+const User = require("../Models/User");
 
-router.get("/",(req, res) => {
-    res.send("hello, this is the user endpoint")
+// UPDATE
+router.put("/:id", async (req, res) => {
+    try {
+        // findByIdAndUpdate return the previous data from DB, thus countered with new: true
+		const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new: true});
+		res.status(200).json(updatedUser);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 })
+
+// DELETE
+router.delete("/:id", async (req, res) => {
+    try {
+		await User.findByIdAndDelete(req.params.id);
+		res.status(200).json("User has been deleted");
+	} catch (error) {
+		res.status(500).json(error);
+	}
+})
+
+// GET
+router.get("/:id",async (req, res) => {
+    try {
+		const Users= await User.findById(req.params.id);
+		res.status(200).json(Users);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+})
+
+// GETALL
+router.get("/",async (req, res) => {
+    try {
+		const Users= await User.find();
+		res.status(200).json(Users);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+})
+
+module.exports = router;
+
 
 module.exports = router;
