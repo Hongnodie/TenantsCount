e065e4668083143fee3d319059120525128e5fee
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-25 21:47:59 +0930

    hotel route

--- Server/Routes/Accomon/hotelRoute.js
+++ Server/Routes/Accomon/hotelRoute.js
@@ -1,11 +1,7 @@
 const router = require("express").Router();
 const Hotel = require("../../Models/Accomon/Hotel");
 
-router.get("/",(req, res) => {
-    res.send("hello, this is the hotel endpoint")
-})
-
-// CREATE+
+// CREATE
 router.post("/", async (req, res) => {
     const newHotel = new Hotel(req.body);
     try {
@@ -17,11 +13,46 @@ router.post("/", async (req, res) => {
 })
 
 // UPDATE
+router.put("/:id", async (req, res) => {
+    try {
+        // findByIdAndUpdate return the previous data from DB, thus countered with new: true
+		const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new: true});
+		res.status(200).json(updatedHotel);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+})
 
 // DELETE
+router.delete("/:id", async (req, res) => {
+    try {
+		await Hotel.findByIdAndDelete(req.params.id);
+		res.status(200).json("Hotel has been deleted");
+	} catch (error) {
+		res.status(500).json(error);
+	}
+})
 
 // GET
+router.get("/:id",async (req, res) => {
+    try {
+		const hotels= await Hotel.findById(req.params.id);
+		res.status(200).json(hotels);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+})
 
 // GETALL
+router.get("/",async (req, res) => {
+    try {
+		const hotels= await Hotel.find();
+		res.status(200).json(hotels);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+})
 
 module.exports = router;
