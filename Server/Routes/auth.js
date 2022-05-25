1b0b30205a8922c6d33fde6b7c03f7ccbf1cf843
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-25 22:29:50 +0930

    login

--- Server/Routes/auth.js
+++ Server/Routes/auth.js
@@ -6,7 +6,7 @@ router.post("/register", async (req, res, next) => {
     try {
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password,salt);
-        
+
 		const newUser = await User({
             username: req.body.username,
             email: req.body.email,
@@ -14,6 +14,20 @@ router.post("/register", async (req, res, next) => {
         });
         await newUser.save();
 		res.status(200).json("User has been created successfully");
+	} catch (error) {
+		res.status(500).json(error);
+	}
+})
+
+router.post("/login", async (req, res, next) => {
+    try {
+        const user = await User.findOne({username:req.body.username});
+        if(!user) return res.status(500).json("user not found");
+
+        const passwordCorrect = await bcrypt.compare(req.body.password, user.password);
+        if(!passwordCorrect) return res.status(500).json("password is incorrect");
+
+		res.status(200).json(user);
 	} catch (error) {
 		res.status(500).json(error);
 	}
