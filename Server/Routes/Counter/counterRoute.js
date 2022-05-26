5027a458190c3f5480b4589b568ba29da59d540e
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-26 19:09:34 +0930

    counter by type

--- Server/Routes/Counter/counterRoute.js
+++ Server/Routes/Counter/counterRoute.js
@@ -17,14 +17,18 @@ router.get("/city",async (req, res) => {
 })
 
 // Hotels by type
-router.get("/type",async (req, res) => {
-    // get cityname string from URL(/?cities=London,Berlin,Singapole)
-    const types = req.query.cities.split(",")
+router.get("/type", async (req, res) => {
+    const typeOptions = ["hotel","studio","apartment","cabin","house","townhouse"];
     try {
-		const list = await Promise.all(cities.map(city=>{
-            return HotelModel.countDocuments({hotelcity:city})
+		let typeCounttest = await Promise.all(typeOptions.map(types=>{
+            let typesNumber = HotelModel.countDocuments({acctype:types});
+            return typesNumber;
         }))
-		res.status(200).json(list);
+        let typeList = [];
+        for (let i =0; i<typeOptions.length-1; i++) {
+            typeList.push({type: typeOptions[i], count: typeCounttest[i]})
+        }
+		res.status(200).json(typeList);
 	} catch (error) {
 		res.status(500).json(error);
 	}
