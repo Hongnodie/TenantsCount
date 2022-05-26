96337dc09c9bf82522278c0c5527408092feb7d2
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-26 13:28:31 +0930

    shirft to export

--- Server/Other/Utilities/token.js
+++ Server/Other/Utilities/token.js
@@ -1,25 +1,23 @@
 const jwt = require('jsonwebtoken');
 
-const verifyToken = (res, req, next) => {
-    // console.log(req.req.cookies);
-    const token =  req.req.cookies.access_token;
-    if(!token) {
-        return res.status(500).json("You are not authenticated");
-    }
-
-    jwt.verify(token, process.env.SECRET_KEY, (err, decodedToken)=>{
-        if(err) return res.status(403).json("Secret Key didn't pass check");
-        // console.log(decodedToken);
-        req.username = decodedToken.data.username;
-        // console.log(req.username);
-        next()
-    })
-}
-
 module.exports = {
     signToken: function ({ username, isAdmin }) {
         const payload = { username, isAdmin };
         return jwt.sign({ data: payload }, process.env.SECRET_KEY, { expiresIn: process.env.TOEKN_EXPIRE });
     },
-    verifyToken
+    verifyToken: function (res, req, next) {
+        // console.log(req.req.cookies);
+        const token =  req.req.cookies.access_token;
+        if(!token) {
+            return res.status(500).json("You are not authenticated");
+        }
+    
+        jwt.verify(token, process.env.SECRET_KEY, (err, decodedToken)=>{
+            if(err) return res.status(403).json("Secret Key didn't pass check");
+            // console.log(decodedToken);
+            req.username = decodedToken.data.username;
+            // console.log(req.username);
+            next()
+        })
+    }
 };
