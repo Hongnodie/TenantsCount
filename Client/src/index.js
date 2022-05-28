438952f790a37fa3f9cfd4617b2f83690e512b3d
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-28 12:04:34 +0930

    use dispatcher

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { SCP } from './context/dispatchContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//  APPLY dispatcher through the app
 root.render(
   <React.StrictMode>
-    <App />
+    <SCP>
+      <App />
+    </SCP>
   </React.StrictMode>
 );
