f9f729095487f10894156d974ce8f087d61a96c2
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-30 08:46:13 +0930

    implement to body component

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AuthContextProvider } from './context/authContext';
 import { SerachOptionProvider } from './context/dispatchContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 //  APPLY context through the app
 root.render(
   <React.StrictMode>
-    <SerachOptionProvider>
-      <App />
-    </SerachOptionProvider>
+    <AuthContextProvider>
+      <SerachOptionProvider>
+        <App />
+      </SerachOptionProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
