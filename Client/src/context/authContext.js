23abc3f5e1412f8f7eb7512454fb0d7c5c38709c
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-30 08:51:48 +0930

    corrected export

--- Client/src/context/authContext.js
+++ Client/src/context/authContext.js
@@ -1,41 +1,34 @@
 import { createContext, useReducer, useEffect } from "react";
 
-// SETUP/INITIATE "STATE" PROPERTY TO BE SPREAD AMONG COMPONENTS
 const INITIAL_STATE ={
     user: JSON.parse(localStorage.getItem("user")) || null,
     onLoading: false,
     error: null
 };
 
-// USE STATE ANYWHERE - PARSE TO ALL COMPONENTS
 export const authContext = createContext(INITIAL_STATE)
 
-// RENEW STATE BASED ON GIVEN HEADER - as ".type"
 const authActionResponder = (state, action) => {
     switch (action.type) {
         case "startlogin":
-            // All required params from search bar bundled as payload when parsing
             return {
                 user: null,
                 onLoading: true,
                 error: null
             };
         case "loginSuccess":
-            // All required params from search bar bundled as payload when parsing
             return {
                 user: action.payload,
                 onLoading: false,
                 error: null
             };
         case "loginFail":
-            // All required params from search bar bundled as payload when parsing
             return {
                 user: null,
                 onLoading: false,
                 error: action.payload
             };
         case "logout":
-            // All required params from search bar bundled as payload when parsing
             return {
                 user: null,
                 onLoading: false,
@@ -59,9 +52,10 @@ export const AuthContextProvider = ({children}) => {
     return (
         <authContext.Provider 
             value={{
-                user: state.user,
-                onLoading: state.loading,
-                error: state.error
+                user: authState.user,
+                onLoading: authState.loading,
+                error: authState.error,
+                authDispatcher
             }}
         >
                 {children}
