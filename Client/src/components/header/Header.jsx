a6c1367b557e75395b10db1ad575d14bc52bbf46
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-30 10:43:39 +0930

    login/out view point

--- Client/src/components/header/Header.jsx
+++ Client/src/components/header/Header.jsx
@@ -1,16 +1,20 @@
 // ICON & LOGIN/OUT
-
+import {useContext} from "react";
+import {authContext} from "../../context/authContext";
 import "./header.css"
 
 const Header = () => {
+
+  const {user, onLoading, error, authDispatcher} =useContext(authContext);
+
   return (
     <div className="navbar">
       <div className="navContainer">
         <span className="logo"><a href="/" style={{color:"inherit", textDecoration:"none"}}>TenantsCount</a></span>
-        <div className="navItems">
+        {user ? (" Welcome "+ user.username) : (<div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
-        </div>
+        </div>)}
       </div>
     </div>
   )
