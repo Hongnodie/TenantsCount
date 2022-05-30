211dcd4bc55eca5c09567506d0cdc43803fad03e
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-30 11:49:58 +0930

    simple routing

--- Server/Routes/Mapview/pinRoute.js
+++ Server/Routes/Mapview/pinRoute.js
@@ -13,7 +13,14 @@ router.post("/", async (req, res) => {
 });
 
 //GET ALL PINS
-
+router.get("/", async (req, res) => {
+    try {
+        const pins = await Pin.find();
+        res.status(200).json(pins);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 //EXPORT ROUTES
 module.exports = router;
