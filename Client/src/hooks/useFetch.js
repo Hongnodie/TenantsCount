b1c7f89d33e77f22aae20b5b0ac5dac69d361962
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-30 20:24:42 +0930

    Latest

--- Client/src/hooks/useFetch.js
+++ Client/src/hooks/useFetch.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import axios from "axios";
 
-axios.defaults.baseURL = '/';
+axios.defaults.baseURL = 'http://localhost:5000';
 
 const useFetch = (url) => {
     const [data, setData] = useState([]);
