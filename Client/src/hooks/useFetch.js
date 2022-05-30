03f7afe4210580f5ffd2f44989a62843fb8b0901
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-30 13:17:55 +0930

    changed build script

--- Client/src/hooks/useFetch.js
+++ Client/src/hooks/useFetch.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = '/';
 
 const useFetech = (url) => {
     const [data, setData] = useState([]);
