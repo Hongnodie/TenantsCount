421cafde671b352d3844606c32be0e22e4a877b4
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-30 15:05:13 +0930

    Server markdown

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,15 @@
+// INSTALL DEPENDENCIES
 const express = require("express");
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config({path: __dirname + '/.env'});
+// console.log(process.env);
 
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
+// TRIGGER PAGE COMPONENT
 const userRoute = require('./Routes/userRoute');
 const authRoute = require('./Routes/auth');
 const hotelRoute = require('./Routes/Accomon/hotelRoute');
@@ -15,24 +18,23 @@ const householdRoute = require('./Routes/Accomon/householdRoute');
 const pinRoute = require('./Routes/Mapview/pinRoute');
 const counterRoute = require('./Routes/Counter/counterRoute');
 
-
 // MIDDLEWARE - parsing object
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
+// SERVER DEPLOY TEST
 // app.get('/',(req,res)=> {
 // res.send("Server is running");
 // });
 
-// console.log(process.env);
+// SERVER CONNECTION
 const connect = async () => {
     await mongoose
     .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> { console.log('mongoDB connected'); })
     .catch((err)=>{console.log(err)});
 }
-
 // TRACK Disconnect or re-connect
 // mongoose.connection.on("disconnected", ()=> {
 //     console.log("mongoDB disconnected")
@@ -50,7 +52,7 @@ app.use('/household', householdRoute);
 app.use('/mapview', pinRoute);
 app.use('/counter', counterRoute);
 
-
+// BUILD APP AS "BUILD" FILE - FOR HEROKU DEPLOY
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(__dirname + '/../Client/build'));
 
@@ -59,6 +61,7 @@ if(process.env.NODE_ENV === 'production'){
     })
 }
 
+// START BACKEND SERVER
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     connect();
