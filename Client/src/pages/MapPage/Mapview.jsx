f971c39c9448552f9ebf7a052fea101a19f15c73
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-30 15:54:46 +0930

    mapview styling

--- Client/src/pages/MapPage/Mapview.jsx
+++ Client/src/pages/MapPage/Mapview.jsx
@@ -1,24 +1,28 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
- 
+import "./mapview.css";
+import SearchBar from "../../components/SearchBar/SearchBar";
+
+// TODO: HIDE
 mapboxgl.accessToken = "pk.eyJ1IjoiZ3JhbnRnYW4iLCJhIjoiY2wzaWd1bzJuMDJhbjNwcGQzbHZ2aW9ocSJ9.U-DLPr6SzIG6LYQ-Q5qhRw";
  
 export default function Mapview() {
-const mapContainer = useRef(null);
-const map = useRef(null);
-const [lng, setLng] = useState(-70.9);
-const [lat, setLat] = useState(42.35);
-const [zoom, setZoom] = useState(9);
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(138.599503);
+    const [lat, setLat] = useState(-34.921230);
+    const [zoom, setZoom] = useState(12);
  
-useEffect(() => {
-if (map.current) return; // initialize map only once
-map.current = new mapboxgl.Map({
-container: mapContainer.current,
-style: 'mapbox://styles/mapbox/streets-v11',
-center: [lng, lat],
-zoom: zoom
-});
-});
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            // TODO: HIDE
+            style: 'mapbox://styles/grantgan/cl3igxdwk000a16pfq05ibitz',
+            center: [lng, lat],
+            zoom: zoom
+        });
+    });
  
 useEffect(() => {
 if (!map.current) return; // wait for map to initialize
@@ -30,11 +34,12 @@ setZoom(map.current.getZoom().toFixed(2));
 });
  
 return (
-<div>
-<div className="sidebar">
-Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-</div>
-<div ref={mapContainer} className="map-container" />
-</div>
+    <div>
+        <SearchBar />
+        <div className="map-section">
+            <div className="sidebar"> Longitude: {lng} | Latitude: {lat} | Zoom: {zoom} </div>
+            <div ref={mapContainer} className="map-container" />
+        </div>
+    </div>
 );
 }
